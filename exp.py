bb0a95665449e05e41247698bcca912f8f645cd1
aditya dey <adiitya.dey@gmail.com>
2026-02-17 12:08:40 +0100

    Fourth Commit.

--- exp.py
+++ exp.py
@@ -1,18 +1,40 @@
 import sys
 sys.path.append("..")
 
-from loading_data.build_dataframes import BuildDataset
+from build.build_dataframes import BuildDataframes
+from build.build_datasets import BatteryDataset
+from build.standardization import Standardizer
+
+from torch.utils.data import DataLoader
 
 
 
 class Exp:
 
     def __init__(self):
-        self.dataframes = BuildDataset("Experiments").get_dataframes()
+        window_size= 30
+        stride=1
 
 
+        # Load raw data
+        train_dataframes = BuildDataframes("Experiments/train").get_dataframes()
+        test_dataframes  = BuildDataframes("Experiments/test").get_dataframes()
+        vali_dataframes  = BuildDataframes("Experiments/vali").get_dataframes()
 
+        # Standardize
+        self.scaler = Standardizer()
+        self.scaler.fit(self.train_dataframes)
 
+        train_std = [self.scaler.transform(df) for df in self.train_dataframes]
+        vali_std  = [self.scaler.transform(df) for df in self.vali_dataframes]
+        test_std  = [self.scaler.transform(df) for df in self.test_dataframes]
+
+        # Build datasets
+        self.train_dataset = BatteryDataset(self.train_std, window_size, stride=stride)
+        self.vali_dataset  = BatteryDataset(self.vali_std, window_size, stride=stride)
+        self.test_dataset  = BatteryDataset(self.test_std, window_size, stride=1)
+
+        self.train_loader = DataLoader(self.train_dataset, batch_size=128, drop_last=False, shuffle=True)
 
 
 # window_size = 200
