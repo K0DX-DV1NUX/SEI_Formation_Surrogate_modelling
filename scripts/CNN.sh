81d17847c6953ef914171ee3308470d09d027a82
aditya dey <adiitya.dey@gmail.com>
2026-02-27 16:31:19 +0100

    CNN included.

---
+++ scripts/CNN.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+run_experiment() {
+    seed=$3
+    model="CNN"
+    mode=$1
+    test_dir=$2
+
+    python run_exp.py \
+        --mode $mode \
+        --model $model \
+        --train_dir "Experiments/train" \
+        --vali_dir "Experiments/vali" \
+        --test_dir "Experiments/$test_dir" \
+        --plots_dir "plots" \
+        --checkpoints_dir "checkpoints" \
+        --results_dir "results" \
+        --epochs 50 \
+        --patience 10 \
+        --batch_size 512 \
+        --learning_rate 1e-3 \
+        --loss mse \
+        --num_workers 0 \
+        --window_size 50 \
+        --stride 10 \
+        --seed $seed \
+        --test_model_folder "checkpoints/CNN_$seed"
+        #--test_standardize_path "checkpoints/CNN_43/std_values.json"
+}
+
+# Example usage:
+run_experiment train test1 2021
+run_experiment test test2 2021
+run_experiment test test3 2021
+
+
+run_experiment train test1 2022
+run_experiment test test2 2022
+run_experiment test test3 2022
+
+run_experiment train test1 2023
+run_experiment test test2 2023
+run_experiment test test3 2023
+
+run_experiment train test1 2024
+run_experiment test test2 2024
+run_experiment test test3 2024
+
+run_experiment train test1 2025
+run_experiment test test2 2025
+run_experiment test test3 2025
