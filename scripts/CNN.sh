264fb5adcb053276d62d17917a76c1ad9bde68f8
aditya dey <adiitya.dey@gmail.com>
2026-03-04 16:11:30 +0100

    Scripts are updated.

--- scripts/CNN.sh
+++ scripts/CNN.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 run_experiment() {
-    seed=$3
-    model="CNN"
     mode=$1
-    test_dir=$2
+    model=$2
+    train_dir=$3
+    vali_dir=$4
+    test_dir=$5
+    seed=$6
 
     python run_exp.py \
         --mode $mode \
         --model $model \
-        --train_dir "Experiments/train" \
-        --vali_dir "Experiments/vali" \
-        --test_dir "Experiments/$test_dir" \
+        --train_dir "$train_dir" \
+        --vali_dir "$vali_dir" \
+        --test_dir "$test_dir" \
         --plots_dir "plots" \
         --checkpoints_dir "checkpoints" \
         --results_dir "results" \
@@ -22,30 +24,29 @@ run_experiment() {
         --loss mse \
         --num_workers 0 \
         --window_size 50 \
-        --stride 10 \
+        --stride 5 \
         --seed $seed \
-        --test_model_folder "checkpoints/CNN_$seed"
-        #--test_standardize_path "checkpoints/CNN_43/std_values.json"
+        --test_model_folder "checkpoints/${model}_${seed}"
 }
 
 # Example usage:
-run_experiment train test1 2021
-run_experiment test test2 2021
-run_experiment test test3 2021
+run_experiment train CNN Experiments/train Experiments/vali Experiments/test1 2021
+run_experiment test CNN Experiments/train Experiments/vali Experiments/test2 2021
+run_experiment test CNN Experiments/train Experiments/vali Experiments/test3 2021
 
 
-run_experiment train test1 2022
-run_experiment test test2 2022
-run_experiment test test3 2022
+run_experiment train CNN Experiments/train Experiments/vali Experiments/test1 2022
+run_experiment test CNN Experiments/train Experiments/vali Experiments/test2 2022
+run_experiment test CNN Experiments/train Experiments/vali Experiments/test3 2022
 
-run_experiment train test1 2023
-run_experiment test test2 2023
-run_experiment test test3 2023
+run_experiment train CNN Experiments/train Experiments/vali Experiments/test1 2023
+run_experiment test CNN Experiments/train Experiments/vali Experiments/test2 2023
+run_experiment test CNN Experiments/train Experiments/vali Experiments/test3 2023
 
-run_experiment train test1 2024
-run_experiment test test2 2024
-run_experiment test test3 2024
+run_experiment train CNN Experiments/train Experiments/vali Experiments/test1 2024
+run_experiment test CNN Experiments/train Experiments/vali Experiments/test2 2024
+run_experiment test CNN Experiments/train Experiments/vali Experiments/test3 2024
 
-run_experiment train test1 2025
-run_experiment test test2 2025
-run_experiment test test3 2025
+run_experiment train CNN Experiments/train Experiments/vali Experiments/test1 2025
+run_experiment test CNN Experiments/train Experiments/vali Experiments/test2 2025
+run_experiment test CNN Experiments/train Experiments/vali Experiments/test3 2025
